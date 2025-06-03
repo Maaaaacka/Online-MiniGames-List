9b4b2f288b4c5e6f4d643204cc5ba20d23e7d1e5
Marske <zz0022200@qq.com>
2025-06-03 16:38:40 +0800

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,8 +6,8 @@
         这是一个由梦泽开发、Marske13搭建运维的在线游戏集合网站，将持续收录380余款在线小游戏，总大小1.4GB。
       </v-card-text>
       <v-card-text>
-         Github@YShenZe
-         Github@Maaaaacka
+         <a herf="https://github.com/YShenZe">Github@YShenZe</a >
+         <a herf="https://github.com/Maaaaacka">Github@Maaaaacka</a >
       </v-card-text>
     </v-card>
   </v-container>
