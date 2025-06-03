24d4b66ab7698c5705a8b3973ac3b6fee3dc1ce1
Marske <zz0022200@qq.com>
2025-06-03 15:16:16 +0800

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,9 @@
     <v-card>
       <v-card-title>关于我们</v-card-title>
       <v-card-text>
-        这是一个由梦泽开发、Macka搭建运维的在线游戏集合网站，共380余款游戏，总大小1.4GB，花费30元人民币。
+        这是一个由梦泽开发、Marske13搭建运维的在线游戏集合网站，将持续收录380余款在线小游戏，总大小1.4GB。
+        Github@YShenZe
+        Github@Maaaaacka
       </v-card-text>
     </v-card>
   </v-container>
